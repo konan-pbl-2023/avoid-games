c2539eaddf5c3db776c393138e449c7acef0f4b1
Ryuhei0603 <yoshiryu43@gmail.com>
2023-11-10 18:05:48 +0900

    いぬを少し動かしました。

--- app/src/main/java/com/example/prototypeapi22/gameplay.java
+++ app/src/main/java/com/example/prototypeapi22/gameplay.java
@@ -1,53 +1,25 @@
 package com.example.prototypeapi22;
 
+import android.media.Image;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.prototypeapi22.databinding.GameplayBinding;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class gameplay extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private GameplayBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_gameplay);
 
-        binding = GameplayBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_gameplay);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        Button rightButton = (Button) findViewById(R.id.rightButton);
+        rightButton.setOnClickListener(view -> {
+            ImageView charaImg = (ImageView) findViewById(R.id.charaImg);
+            charaImg.setX(80);
         });
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_gameplay);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
