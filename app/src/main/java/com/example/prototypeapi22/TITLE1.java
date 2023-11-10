2f228924029d0cbb83c39f2d96d0c669bf7c4529
Yuna-11 <yunarino45@icloud.com>
2023-11-10 15:17:02 +0900

    TITLE1コミットしました。

--- app/src/main/java/com/example/prototypeapi22/TITLE1.java
+++ app/src/main/java/com/example/prototypeapi22/TITLE1.java
@@ -16,7 +16,6 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.prototypeapi22.databinding.ActivityTitle1Binding;
 
-
 public class TITLE1 extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
