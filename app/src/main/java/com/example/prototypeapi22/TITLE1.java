7fef0a233167643bc9863adfc930943c998571e9
MatsudaKeijiro <keijiro826@gmail.com>
2023-11-10 17:35:54 +0900

    スタートボタンを押してゲーム画面に画面遷移

--- app/src/main/java/com/example/prototypeapi22/TITLE1.java
+++ app/src/main/java/com/example/prototypeapi22/TITLE1.java
@@ -1,5 +1,6 @@
 package com.example.prototypeapi22;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -28,26 +29,9 @@ public class TITLE1 extends AppCompatActivity {
         binding = ActivityTitle1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_title1);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        binding.start.setOnClickListener((View v) ->
+        {
+            startActivity(new Intent(this, gameplay.class));
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_title1);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
