a128623ee7d5a3ccda71217b80e915ada9316d88
syoutanisikawa <s2271043@s.konan-u.ac.jp>
2023-11-17 16:28:27 +0900

    障害物を追加しました

--- app/src/main/java/com/example/prototypeapi22/gameplay.java
+++ app/src/main/java/com/example/prototypeapi22/gameplay.java
@@ -26,25 +26,68 @@ import android.widget.TextView;
 
 import com.example.prototypeapi22.databinding.GameplayBinding;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class gameplay extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private GameplayBinding binding;
 
     private ImageView jiki;
-    private float initJikiX = 387f;
+    private ImageView syougai;
+    private ImageView obstacle;
+    private float syougaiY;
+    private float initJikiX = 380f;
     private float jikiY;
+    private float initsyougaiY=0f;
+    private float initsyougaiX=380f;
 
+    private int i;
     int cnt = 1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = GameplayBinding.inflate(getLayoutInflater());
         setContentView(R.layout.gameplay);
-
         jiki = (ImageView) findViewById(R.id.jiki);
+        obstacle = (ImageView) findViewById(R.id.syougai);
+
+        syougaiY=initsyougaiY;
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                int syougai[] = {2, 1, 1, 2, 1, 0, 1, 2, 2, 1};
+                if (syougai[i] == 0) {
+                    obstacle.setX(0);
+                }
+                else if (syougai[i] == 1) {
+                    obstacle.setX(380);
+                }
+                else if (syougai[i] == 2) {
+                    obstacle.setX(760);
+                }
+                syougaiY+=10;
+                if(syougaiY==1800){
+                    syougaiY=initsyougaiY;
+                    i++;
+                }
+                if(i==10){
+                    i=0;
+                }
+                obstacle.setY(syougaiY);
+                if(((Math.abs(jiki.getX()-obstacle.getX())<10))&&((Math.abs(jiki.getY()-obstacle.getY())<50))){
+                            next();
+                }
+
+            }
+
+
+        }, 100, 10);
+
 
         findViewById(R.id.hidari).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +101,7 @@ public class gameplay extends AppCompatActivity {
                 }
                 float jikiX = initJikiX * cnt;
                 jiki.setX(jikiX);
+
                 Log.d("cnt", String.valueOf(cnt) + "/pos:"+  String.valueOf(jiki.getX()));
             }
         });
@@ -92,14 +136,11 @@ public class gameplay extends AppCompatActivity {
 
 
 
-        Button button = findViewById(R.id.next);
-        button.setOnClickListener(v ->
-                next()
-        );
+
 
 //        Button button2 = findViewById(R.id.kisetu);
 //        button2.setOnClickListener(v ->
-//        );
+//          );
     }
 
 
